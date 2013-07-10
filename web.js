2e9adec341b1a0fabfb6235f31171e1f42e42530
Simon Howey <simon.howey@gmail.com>
2013-07-10 21:25:07 +0000

    fixed bug3 in web.js

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var responsehtml = buf.toString(fs.readFileSync('index.html'));
+
 
 app.get('/', function(request, response) {
-response.send(fs.readFileSync('index.html', 'utf8'));
+response.send(responsehtml);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
