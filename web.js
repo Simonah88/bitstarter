b9ca8a47e54e7cb66d4c7431b2ecef0d7750114a
Simon Howey <simon.howey@gmail.com>
2013-07-10 21:33:10 +0000

    fixed another bug in web.js zzz deleted something by accident in emacs

--- web.js
+++ web.js
@@ -6,7 +6,10 @@ var responsehtml = buf.toString(fs.readFileSync('index.html'));
 
 
 app.get('/', function(request, response) {
+
 response.send(responsehtml);
+
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
