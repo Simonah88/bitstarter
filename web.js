620954b0316ea7beb466800b291ca3bf66764b06
Simon Howey <simon.howey@gmail.com>
2013-07-10 22:20:16 +0000

    zzz

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 var temphtml = fs.readFileSync('index.html');
 
-var responsehtml = buf.toString(temphtml);
+var responsehtml = temphtml.toString();
 
 
 app.get('/', function(request, response) {
